97cedf616f6273f01fb3b7ad5bff7ff8d427af1c
NickFr3 <151786343+NickFr3@users.noreply.github.com>
2024-04-24 21:25:26 +0200

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    if (typeof(num1) !== 'number' || typeof(num2) !== 'number' || num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } else if (num1 < num2) {
+        let count = num2;
+
+        while (count >= num1) {
+            sum += count;
+            count -= 1;
+        }
+    
+        return sum;
+    } else {
+        let count = num1;
+
+        while (count >= num2) {
+            sum += count;
+            count -= 1;
+        }
+    
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
