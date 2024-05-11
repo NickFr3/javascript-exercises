ae31e02342e96fa334ffa10d047a826ebd0c2a7d
NickFr3 <151786343+NickFr3@users.noreply.github.com>
2024-05-11 14:44:47 +0200

    Excercise complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {    
+    // Filtered array containing only letters and numbers
+    const newString = string
+        .toLowerCase()
+        .split('')
+        .filter((char) => char.match(/[a-z0-9]/))
+        .join('');
+    
+    const reverse = newString.split('').reverse().join('');
 
+    // Join arrays to strings and return comparison
+    return reverse == newString;
 };
 
 // Do not edit below this line
