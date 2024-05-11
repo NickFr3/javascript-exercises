b66ff2e7cf59ad6a6bfb879f88dbcf714e0bae2e
NickFr3 <151786343+NickFr3@users.noreply.github.com>
2024-05-11 15:35:49 +0200

    Excercise complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    const numIndex = Number(countArg);
 
+    if (numIndex < 0) {
+        return "OOPS";
+    } else {
+        // Initialize array with starting numbers
+        let fib = [0, 1];
+    
+        for (i = 0; i <= numIndex - 2; i++) {
+            let nextNum = fib[fib.length - 1] + fib[fib.length - 2];
+            fib.push(nextNum);
+        }
+
+        return fib[numIndex];
+    }
 };
 
 // Do not edit below this line
