fbed818a68c10bea8fbb59bda9b4020749089cae
NickFr3 <151786343+NickFr3@users.noreply.github.com>
2024-05-11 16:29:55 +0200

    Excercise complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    // Get age of person
+    function getAge(birth, death) {
+        if(!death) {
+            death = new Date().getFullYear();
+        }
 
-};
+        return death - birth;
+    }
+
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath)
+
+        if (currentAge > oldestAge) {
+            return currentPerson;
+        } else {
+            return oldest;
+        }
+    })
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
